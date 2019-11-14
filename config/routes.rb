62f7cd88a71ab5c9e76807f06711b8a47032a17a
Jesse LE <jlandiseigsti@gmail.com>
2019-11-14 10:54:19 -0500

    creates controllers Cards and Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards/show'
+  get 'cards/new'
+  get 'cards/edit'
+  get 'cards/create'
+  get 'cards/update'
+  get 'cards/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
